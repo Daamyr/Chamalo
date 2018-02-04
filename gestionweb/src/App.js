9bbc8aa5c3c14bdf7fe3f87dc39595e35581fd16
Maxime Damour <maxime.damour974@gmail.com>
2018-02-03 20:32:35 -0500

    map presque ok

--- gestionweb/src/App.js
+++ gestionweb/src/App.js
@@ -2,18 +2,116 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {GoogleApiWrapper} from 'google-maps-react';
+import SocketIO from 'socket.io-client';
+
+// var SocketIO = require('nativescript-socket.io');
+// let socketio = null;
 
 class App extends Component {
-  // componentDidMount(){
-  //   let map = new window.google.maps.Map(document.getElementById('map'),{
-  //     center: {lat: -33.8688, lng: 151.2195},
-  //     mapTypeControl: true,
-  //     zoom : 15,
-  //     mapTypeId: 'roadmap'
-  //   });
-  // }
 
+  componentDidMount(){
+    console.log("Hello" + this.socketio);
+  }
+
+  constructor(){
+    super();
+    this.socketio = null;
+
+    this.state = {
+      page : "login",
+      username : "",
+      password : "",
+    }
+
+  }
+
+  handleChange (event) {
+      console.log(event.target.value);
+      this.setState({username: event.target.value});
+    }
+
+    setPage = (page) => {
+      this.setState({ page })
+    }
+
+  connect() {
+      console.log("Connection request");
+      try {
+        if (this.socketio != null) {
+          console.log("not null");
+          this.socketio.emit("forceDisconnect");
+          this.socketio = null;
+        }
+
+        this.socketio = SocketIO.connect('http://138.197.172.107:8081?token=prod&username='+ this.state.username +'&password='+ this.state.username);
+
+        console.log(this.socketio);
+
+        this.socketio.emit("Id", {"Id":0});
+
+        this.socketio.on('gestion:coords',
+          function (data){
+            let latitude = data.Latitude;
+            let longitude = data.Longitude;
+            let speed = data.Speed;
+            let direction = data.Direction;
+            let timestamp = data.TimeStamp;
+            console.log(latitude + " " + longitude + " " + speed + " " + direction + " " + timestamp);
+          });
+
+        this.socketio.on('reconnect',
+          function(){
+            console.log("allo reco");
+            this.socketio.emit("Id", {"Id":0});
+          });
+
+        let vm = this;
+        this.socketio.on('connectack',
+          function(){
+            console.log("Auth ok");
+            vm.setPage("normal");//-----------------------------------------
+          });
+
+      } catch (e) {
+        console.log(e);
+      } finally {
+          
+      }
+  }
+    
+
+  test(){
+    if(!this.socketio.connected){
+      alert("you're not connected");
+       }else{
+    this.socketio.emit("test", {"connection":{"prenom":"Maxime", "nom":"Damour"}});
+}
+
+  }
   render() {
+  if(this.state.page === 'login'){
+      return (
+        <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <input id="username"
+        type="text"
+        value={this.state.username}
+        onChange={this.handleChange}
+        />
+        <input  id="password"
+        type="password"
+        value={this.state.password}
+        onChange={this.handleChange}
+        />
+        <button onClick={() => this.connectFromLogin()}>Connection</button>
+        </div>
+        );
+    }
+
+
     return (
       <div className="App">
         <header className="App-header">
@@ -24,7 +122,8 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <h3>allo dans le render</h3>
-        <div id="map" />
+      <button onClick={() => this.connect()}>Connection</button>
+      <button onClick={() => this.test()}>test</button>
       </div>
     );
   }
--- gestionweb/src/MapContainer.js
+++ gestionweb/src/MapContainer.js
@@ -2,13 +2,25 @@ import React, { Component } from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 export class MapContainer extends Component {
+
+  createMapOptions(maps) {
+    console.log("allo");
+  return {
+    gestureHandling: 'cooperative' // Will capture all touch events on the map towards map panning
+  }
+}
+
 render() {
     return (
-      <Map google={this.props.google} zoom={10}
+      <Map 
+        google={this.props.google}
+        zoom={10}
       	initialCenter={{
             lat: 46.545732,
             lng: -72.749542
           }}
+          scrollwheel={false}
+          // gestureHandling="none"
         >
  
         <Marker onClick={this.onMarkerClick}
